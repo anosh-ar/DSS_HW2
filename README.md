c4c7fa1c520e9376ecbe0c58caab92d8ca23a516
Anosh <ahmadanosh.ar@gmail.com>
2023-11-13 23:38:29 +0100

    finnal

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DSS_HW2
+
+This is the main part of code. It develop a for loop. at each loop a qestion in generated
+    and asked from the user. If the user answer the question correctly the score adds up 1.
+    at the end user can see its score from total possible achievable score.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='math_quiz',
+   version='1.0',
+   description='A Math Quiz Game',
+   author='Anosh',
+   author_email='ahmadanosh.ar@gmail.com',
+   packages=['math_quiz'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
